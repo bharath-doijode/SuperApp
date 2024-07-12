f689daeb543c7f3a0e0ed867cdc10ddabaf04328
bharath.doijode <bharath.doijode@olamnet.com>
2024-07-12 12:15:09 +0530

    updated to point prod catalog

--- packages/host/index.js
+++ packages/host/index.js
@@ -16,7 +16,7 @@ ScriptManager.shared.setStorage(AsyncStorage);
 ScriptManager.shared.addResolver(async (scriptId, caller) => {
 
   const containersURL = getContainersURL({
-    hostname: "http://localhost:3000",//"https://catalog-server-opa.vercel.app/",//process.env.SAS_CATALOG_SERVER_URL,
+    hostname: "https://catalog-server-opa.vercel.app/",//"http://localhost:3000",//"https://catalog-server-opa.vercel.app/",//process.env.SAS_CATALOG_SERVER_URL,
     version: appVersion,
     platform: Platform.OS,
     appName,
